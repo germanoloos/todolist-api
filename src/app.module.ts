246e7a2151c0be301b054f8ce8edaa7f5a3c0156
Germano Loos <germanoloos@hotmail.com>
2022-06-24 17:40:42 -0300

    DB connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      url: process.env.DATABASE_URL,
       entities: [],
       synchronize: true,
+      ssl: true,
     }),
   ],
   controllers: [AppController],
