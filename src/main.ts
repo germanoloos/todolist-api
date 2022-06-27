026761422832a2549203fdcf5a74c2d7b2fb245b
Germano Loos <germanoloos@hotmail.com>
2022-06-26 23:05:14 -0300

    Environment setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { fastifyPlugin as rTracerPlugin } from 'cls-rtracer';
+import { fastifyHelmet as helmetPlugin } from 'fastify-helmet';
+import multipartPlugin from 'fastify-multipart';
+import hexoid from 'hexoid';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(helmet());
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { logger: false },
+  );
+  app.register(helmetPlugin);
+  app.register(rTracerPlugin, {
+    echoHeader: true,
+    requestIdFactory: hexoid(32),
+  });
+  app.register(multipartPlugin);
   await app.listen(process.env.PORT);
 }
 bootstrap();
