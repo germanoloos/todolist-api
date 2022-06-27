69b23c3680896ce954e1f30fc08e298ecce9e3c7
Germano Loos <germanoloos@hotmail.com>
2022-06-26 22:45:59 -0300

    Environment setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:4200/', 'https://todolist-web-eta.vercel.app'],
+  });
+  console.log('AppModule started');
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
